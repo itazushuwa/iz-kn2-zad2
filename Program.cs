a35777b7ae5982d4c3fc74da68667e659d668d55
Vukkert <Vukkert@DESKTOP-FMS6LDH>
2022-12-02 19:06:17 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+Random rnd = new Random(DateTime.Now.Millisecond);
+Console.WriteLine("Размер массива:");
+int[] arr = new int[int.Parse(Console.ReadLine())];
+int min = 0, max = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = rnd.Next(-101, 100);
+    Console.Write(arr[i] + ", ");
+    if (arr[i] >= arr[max]) max = i;
+    if (arr[i] <= arr[min]) min = i;
+}
+Console.WriteLine();
+int swap = arr[min];
+arr[min] = arr[max];
+arr[max] = swap;
+foreach (var c in arr)
+{
+    Console.Write(c + ", ");
+}
